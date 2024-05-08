b42abbd918f53abc2c108b927113a6878852447a
cmigliaccio00 <44817903+cmigliaccio00@users.noreply.github.com>
2024-05-08 15:41:49 +0200

    Iniziato esercizio 2 lab3

--- Laboratori/lab3/src/es0302.rs
+++ Laboratori/lab3/src/es0302.rs
@@ -1,6 +1,12 @@
-
 use std::time::SystemTime;
+use crate::es0302::FSError::NotFound;
 
+/*----------------------------------------------------------------
+Un nodo dell'albero (associato al File System) può essere:
+    --> Un File (tipo File)
+    --> Una Cartella (tipo Dir): questa può contenere altre
+    cartelle o dei file
+----------------------------------------------------------------*/
 struct File {
     name: String,
     modified: SystemTime,
@@ -19,6 +25,7 @@ enum Node {
     Dir(Dir),
 }
 
+#[derive(Debug)]
 enum FSError {
     NotFound,     // file or dir not found
     NotADir,      // when trying to ad children to a file
@@ -28,10 +35,10 @@ enum FSError {
 }
 
 // define lifetimes
-struct MatchResult {
-    q: &str, // matched query string
+struct MatchResult<'a, 'b> {
+    q: &'a str, // matched query string
     path: String, // matched path
-    node: &Node, // matched node
+    node: &'b Node, // matched node
 }
 
 struct Filesystem {
@@ -42,76 +49,144 @@ impl Filesystem {
     // create a new empty filesystem with a root dir
     // (name of the root dir is empty string: "")
     pub fn new() -> Self {
-        unimplemented!()
+        Filesystem{root: Node::Dir( Dir{  name: "".to_string(),
+                                    modified: SystemTime::now(),
+                                    children: Vec::new()
+                                }
+                            )
+                }
     }
 
+    /*
     // create a new filesystem reading from disk all the structure under the given path
     // in the file content just write the firt 1k bytes of the file
     // return the root node of the filesystem
-    // (implement this function at the end, after all the other methods, the only purpose is to take a look std::fs functions, use std::fs:read_dir)
+    // (implement this function at the end, after all the other methods,
+    // the only purpose is to take a look std::fs functions, use std::fs:read_dir)
     pub fn from(path: &str) -> Self {
         unimplemented!()
     }
+    */
 
     // create a new directory in the filesystem under the given path
-    // return a reference the created dir
+    // return a reference to the created dir
     // possible errors: NotFound, path NotADir, Duplicate
     pub fn mkdir(&mut self, path: &str, name: &str) -> Result<&mut Dir, FSError> {
-        unimplemented!()
+
+        let steps:Vec<_> = path.split("/").collect();
+
+        //Sono nella radice
+        if (path.to_string()=="/".to_string()){
+            //Devo controllare se non c'è già
+             match &self.root{
+                 Node::File(File) => (),
+                 Node::Dir(mut Dir) => {
+                     //Vedi se per caso la cartella già esiste
+                    for nodo in Dir.children.iter(){
+                        match nodo{
+                            Node::File(i) => {
+                                if (i.name == name){ return Err(FSError::NotADir) }
+                            },
+                            Node::Dir(d) =>{
+                                if d.name == name{  return Err(FSError::Duplicate); }
+                            }
+                        }
+                    }
+                     //Se è andato tutto bene creo la directory
+                    Dir.children.push( Node::Dir(Dir{ name: String::from(name),
+                                                    modified:SystemTime::now(),
+                                                    children: Vec::new()}));
+                 }
+             }
+        }
+        else{
+            let mut flag=0;
+            //Primo livello
+            let mut next = &self.root;
+            //Ad ogni livello del path
+            for step in steps{
+                match next{
+                    Node::File(f) => {
+                        if f.name==step{ return Err(FSError::NotADir)} },
+                    Node::Dir(d) => {
+                        if d.name==step{
+                            //Cerca la cartella
+                            flag=0;
+                            for dir in d.children.iter(){
+                                match dir{
+                                    Node::Dir(dd) =>{
+                                        if(dd.name==step){ next=dir; } },
+                                    _ => ()
+                                }
+                            }
+                            //Cartella non trovata
+                            if !flag{ return Err(FSError::NotFound)}
+                        }
+                    }
+                }
+            }
+
+            //Sono arrivato a destinazione controllo se la cartella non esiste e la creo
+        }
+        Err(FSError::DirNotEmpty)
     }
+
 
     // possible errors: NotFound, path is NotADir, Duplicate
     pub fn create_file(&mut self, path: &str, name: &str) -> Result<&mut File, FSError> {
         unimplemented!()
     }
 
-    // updated modification time of the file or the dir
-    // possible errors: NotFound
-    pub fn touch(&mut self, path: &str) -> Result<(), FSError> {
-        unimplemented!()
-    }
+    /*
+   // updated modification time of the file or the dir
+   // possible errors: NotFound
+   pub fn touch(&mut self, path: &str) -> Result<(), FSError> {
+       unimplemented!()
+   }
 
-    // remove a node from the filesystem and return it
-    // if it's a dir, it must be empty
-    // possible errors: NotFound, DirNotEmpty
-    pub fn delete(&mut self, path: &str) -> Result<Node, FSError> {
-        unimplemented!()
-    }
+   // remove a node from the filesystem and return it
+   // if it's a dir, it must be empty
+   // possible errors: NotFound, DirNotEmpty
+   pub fn delete(&mut self, path: &str) -> Result<Node, FSError> {
+       unimplemented!()
+   }
 
-    // get a reference to a node in the filesystem, given the path
-    pub fn get(&mut self, path: &str) -> Result<&Node, FSError> {
-        unimplemented!()
-    }
+   // get a reference to a node in the filesystem, given the path
+   pub fn get(&mut self, path: &str) -> Result<&Node, FSError> {
+       unimplemented!()
+   }
 
-    // get a mutable reference to a node in the filesystem, given the path
-    pub fn get_mut(&mut self, path: &str) -> Result<&mut Node, FSError> {
-        unimplemented!()
-    }
+   // get a mutable reference to a node in the filesystem, given the path
+   pub fn get_mut(&mut self, path: &str) -> Result<&mut Node, FSError> {
+       unimplemented!()
+   }
 
-    // search for a list of paths in the filesystem
-    // qs is a list query strings with constraints
-    // the constraints must be matched in or (it's returned any node matching at least one constraint)
-    // constraint format: "type:pattern"
-    // constraints:
-    // - "type:dir" -> match only directories
-    // - "type:file" -> match only files
-    // - "name:value" -> match only nodes with the given name
-    // - "partname:value" -> match only nodes with the given string in the name
+   // search for a list of paths in the filesystem
+   // qs is a list query strings with constraints
+   // the constraints must be matched in or (it's returned any node matching at least one constraint)
+   // constraint format: "type:pattern"
+   // constraints:
+   // - "type:dir" -> match only directories
+   // - "type:file" -> match only files
+   // - "name:value" -> match only nodes with the given name
+   // - "partname:value" -> match only nodes with the given string in the name
 
-    pub fn find<'a>(&'a self, qs: &[&'a str]) -> Vec<MatchResult> {
-        unimplemented!()
-    }
+   pub fn find<'a>(&'a self, qs: &[&'a str]) -> Vec<MatchResult> {
+       unimplemented!()
+   }
 
 
-    // walk the filesystem, starting from the root, and call the closure for each node with its path
-    // the first parameter of the closure is the path of the node, second is the node itself
-    pub fn walk(&self, f: impl Fn(&str, &Node)) {
-        unimplemented!()
-    }
+   // walk the filesystem, starting from the root, and call the closure for each node with its path
+   // the first parameter of the closure is the path of the node, second is the node itself
+   pub fn walk(&self, f: impl Fn(&str, &Node)) {
+       unimplemented!()
+   }
+    */
 }
 
-fn demo() {
+pub fn demo() {
 
+    //Creazione del File System
     let mut fs = Filesystem::new();
 
     // create a directory structure, 10 dirs with a child dir and file each one
@@ -121,6 +196,7 @@ fn demo() {
         fs.create_file(format!("/dir{}", i).as_str(), "file1").unwrap();
     }
 
+    /*
     println!("find /child2");
     if let Ok(res) = fs.get("/dir2/child1") {
         match res {
@@ -182,6 +258,6 @@ fn demo() {
             }
         }
     });
-
+*/
 }
 
--- Laboratori/lab3/src/main.rs
+++ Laboratori/lab3/src/main.rs
@@ -1,20 +1,32 @@
 use crate::es0301::{demo1, demo2, demo3, demo4, demo_SimpleDNAIter, demo_dna_iter, demo_dna_iter2};
+use crate::es0302::{demo};
 
 mod es0301;
+mod es0302;
 
 fn main() {
-    println!("demo1");
-    demo1();
-    println!("demo2");
-    demo2();
-    println!("demo3");
-    demo3();
-    println!("demo4");
-    demo4();
-    println!("demo_SimpleDNAIter");
-    demo_SimpleDNAIter();
-    println!("demo_dna_iter");
-    demo_dna_iter();
-    println!("demo_dna_iter2");
-    demo_dna_iter2();
+
+    let What=2;
+
+    if What==1{
+        //Esercizio 1 - Lifetime, uso di Iteratori
+        println!("demo1");
+        demo1();
+        println!("demo2");
+        demo2();
+        println!("demo3");
+        demo3();
+        println!("demo4");
+        demo4();
+        println!("demo_SimpleDNAIter");
+        demo_SimpleDNAIter();
+        println!("demo_dna_iter");
+        demo_dna_iter();
+        println!("demo_dna_iter2");
+        demo_dna_iter2();
+    }
+    else{
+        //Esercizio 2
+        demo();
+    }
 }
